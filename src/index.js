0d1cab81869f23cb91ea8771f507eed692b40705
Olga Papkovskaya <o.papkovskaya@gmail.com>
2018-02-22 03:29:20 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let n = 0;
+  while (number > 0){
+    number = Math.floor(number / 5);
+    n += number;
+  }
+  return n;
 }
